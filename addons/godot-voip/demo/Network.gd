6f93cb27200ba443c57b9bc24836eb83de06fcb1
Mason.S <meisamsafavi5@gmail.com>
2022-05-25 12:11:47 +0430

    Add files via upload

--- addons/godot-voip/demo/Network.gd
+++ addons/godot-voip/demo/Network.gd
@@ -1,28 +1,34 @@
 extends Node
 class_name Network
 
-var server_port := 1909
+var server_port := 3000
 var server_ip := "127.0.0.1"
-var max_players = 100
 
-var network = NetworkedMultiplayerENet.new()
+
 
 
 func start_client() -> int:
-	
-	network.create_client(server_ip,server_port)
-	get_tree().set_network_peer(network)
-	
+	var peer := NetworkedMultiplayerENet.new()
+
+	var err := peer.create_client(server_ip,server_port)
+	if err != OK:
+		return err
+
+	get_tree().set_network_peer(peer)
+
 	return OK
 
 func start_server() -> int:
-	
-	
-	network.create_server(server_port,max_players)
-	get_tree().set_network_peer(network)
-	
-	
+	var peer := NetworkedMultiplayerENet.new()
+
+	var err := peer.create_server(server_port,5)
+
+	if err != OK:
+		return err
+
+	get_tree().set_network_peer(peer)
+
 	return OK
 
 func stop() -> void:
-	pass
+	print("Please close application to disconnect :) ")
